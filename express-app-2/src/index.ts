9b4a5f08244d3d34afb02a2e772e1d44b29e6391
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-09-02 23:15:00 +0530

    added tests for express app

--- express-app-2/src/index.ts
+++ express-app-2/src/index.ts
@@ -4,6 +4,8 @@ import express from "express"
 // we dont want to start a server when testing
 //we use supertest liberary which helps you to send http requests and get the response without starting a server.
 
+//we can also use app.linten here and test using axios ,  but supertest liberary in better
+
 export const app = express()
 
 app.use(express.json())
@@ -12,12 +14,12 @@ app.use(express.json())
 app.post('/sum', (req, res) => {
     const {a, b} = req.body;
     const ans = a + b;
-    res.json({ans})
+    res.json({ans}).status(200)
 })
 
 app.post('/mul', (req, res) => {
     const {a, b} = req.body;
     const ans = a * b;
-    res.json({ans})
+    res.json({ans}).status(200)
 })
 
